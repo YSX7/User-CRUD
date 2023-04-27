4117cb06f7a3b97b143f5fb7e08e4693a3200c12
Yurii Semenov <ysx080@gmail.com>
2023-04-27 19:17:09 +0500

    feat: sessions

---
+++ backend/pkg/sessions/sessions.go
@@ -0,0 +1,33 @@
+package sessions
+
+import (
+	"crypto/rsa"
+
+	"vuegolang/graph/model"
+)
+
+type Session struct {
+	Id    string
+	Login string
+	model.Role
+	PrivateKey rsa.PrivateKey
+}
+
+type sessions map[string]Session
+
+func (s sessions) Remove(tokenStr string) {
+	delete(s, tokenStr)
+}
+
+func (s sessions) Get(tokenStr string) (Session, bool) {
+	value, ok := s[tokenStr]
+	return value, ok
+}
+
+func (s sessions) Add(tokenStr string, session *Session) {
+	s[tokenStr] = *session
+}
+
+func New() Interface {
+	return &sessions{}
+}
