ddbbec6e867c0d1b690fbfbb0b99f585da9b65fa
Yurii Semenov <ysx080@gmail.com>
2023-05-18 20:30:59 +0500

    feat: routes

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,18 +1,17 @@
-import MainLayoutVue from 'layouts/MainLayout.vue';
-import RedirectBeachVue from 'pages/RedirectBeach.vue';
 import { RouteRecordRaw } from 'vue-router';
+
+const MainLayoutComponent = () => import('layouts/MainLayout.vue')
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ name:'login', path: '', component: () => import('pages/IndexPage.vue') }],
+    component: MainLayoutComponent,
+    children: [
+      { name: 'login', path: '/login', component: () => import('src/pages/LoginPage.vue'), meta: { requiresAuth: false } },
+      { name: 'index', path: '/', component: () => import('src/pages/IndexPage.vue'), meta: { requiresAuth: true } },
+      { name: 'users', path: '/users', component: () => import('src/pages/UsersPage.vue'), meta: { requiresAuth: true } },]
   },
-  {
-    path: '/redirectBeach',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ name: 'index', path: '', component: () => import('pages/RedirectBeach.vue')   }],
-  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
